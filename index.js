bc70f613932811fd9a95ba9cf70506f485fbbcff
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-06 06:23:32 +0200

    project random fighters

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ let stageEl = document.getElementById("stage");
 let fightBtn = document.getElementById("fightBtn");
 
 fightBtn.addEventListener("click", function () {
-  let pickFighterOne = Math.floor(Math.random() * 16) + 1;
-  let pickFighterTwo = Math.floor(Math.random() * 16) + 1;
+  let pickFighterOne = Math.floor(Math.random() * fighters.length) + 1;
+  let pickFighterTwo = Math.floor(Math.random() * fighters.length) + 1;
   stageEl.textContent =
     fighters[pickFighterOne] + " VS" + fighters[pickFighterTwo];
 });
